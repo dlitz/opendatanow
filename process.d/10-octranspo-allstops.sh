19c17fe89bdf471cbe4e0da4b4aa89d5a0c65cfb
Dwayne C. Litzenberger <dlitz@dlitz.net>
2010-04-10 13:08:22 -0400

    Make scraped output more descriptive

--- process.d/10-octranspo-allstops.sh
+++ process.d/10-octranspo-allstops.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+scrape_url='http://www.octranspo1.com/google/retrieve_stops?zoom=15&ne=47.0000,-74.0000&sw=44.0000,-77.0000'
+
 # Fetch OC Transpo stops if they haven't already been fetched
+# Original data from http://www.octranspo1.com/maps/
 if ! [ -f repo/octranspo/raw/allstops.bin ] ; then
     mkdir -p repo/octranspo/raw
-    wget -q -Otmp/octranspo-allstops.bin 'http://www.octranspo1.com/google/retrieve_stops?zoom=15&ne=47.0000,-74.0000&sw=44.0000,-77.0000'
+    wget -q -Otmp/octranspo-allstops.bin "$scrape_url"
     mv -f tmp/octranspo-allstops.bin repo/octranspo/raw/allstops.bin
 fi
 
@@ -13,7 +16,13 @@ if ! [ -f repo/octranspo/allstops.xml.gz ] || [ repo/octranspo/allstops.xml.gz -
     mkdir -p repo/octranspo
 
     # Add XML preamble and convert to UTF-8
-    echo '<?xml version="1.0" encoding="UTF-8"?>' > tmp/octranspo-allstops.xml
+    cat > tmp/octranspo-allstops.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Scraped `date -R`
+     from $scrape_url
+     Original data at: http://www.octranspo1.com/maps/ -->
+EOF
+
     iconv -f latin1 -t utf-8 repo/octranspo/raw/allstops.bin >> tmp/octranspo-allstops.xml
 
     # compress using gzip
