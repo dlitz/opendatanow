b3744d0af8ba84169d62bf6f69e2ced8419c2601
Dwayne C. Litzenberger <dlitz@dlitz.net>
2010-04-10 13:40:01 -0400

    csv: Remove made-up header

--- support/octranspo-stops-to-csv.py
+++ support/octranspo-stops-to-csv.py
@@ -15,12 +15,11 @@ class Handler(xml.sax.handler.ContentHandler):
             self.csv_writer.writerow(map(lambda c: attrs[c].encode("UTF-8"), self.cols))
 
 cols = [u"id", u"stopid", u"lat", u"lng", u"name"]
-header = [u"stopNum", u"stopCode", u"latitude", u"longitude", u"name"]
 input_filename = sys.argv[1]
 input_file = open(input_filename, "rb")
 output_file = sys.stdout
 csv_writer = csv.writer(output_file)
-csv_writer.writerow(map(lambda c: c.encode("UTF-8"), header))
+csv_writer.writerow(map(lambda c: c.encode("UTF-8"), cols))
 
 xml.sax.parse(input_file, Handler(csv_writer, cols))
 
